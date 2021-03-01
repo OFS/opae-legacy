9b93194498c2e1185e3c382b8cda30cfa325d732
Tim Whisonant <tim.whisonant@intel.com>
2021-03-01 12:18:26 -0800

    Fix checker issues.

--- tools/fpgad/monitor_thread.c
+++ tools/fpgad/monitor_thread.c
@@ -1,4 +1,4 @@
-// Copyright(c) 2018-2019, Intel Corporation
+// Copyright(c) 2018-2021, Intel Corporation
 //
 // Redistribution  and  use  in source  and  binary  forms,  with  or  without
 // modification, are permitted provided that the following conditions are met:
@@ -110,7 +110,7 @@ STATIC void mon_monitor(fpgad_monitored_device *d)
 	}
 }
 
-STATIC volatile bool mon_is_ready = false;
+STATIC volatile bool mon_is_ready = (bool)0;
 
 bool monitor_is_ready(void)
 {
